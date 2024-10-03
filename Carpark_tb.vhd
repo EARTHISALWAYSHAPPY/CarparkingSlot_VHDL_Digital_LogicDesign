f6edc4a84b49c87e46a8a5e49730494049b2e4cd
EARTHISALWAYSHAPPY <107309420+EARTHISALWAYSHAPPY@users.noreply.github.com>
2024-10-03 15:08:36 +0700

    Update Carpark_tb.vhd

--- Carpark_tb.vhd
+++ Carpark_tb.vhd
@@ -2,52 +2,55 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-
+--Set Testbench for Parking_Counter.
 entity Parking_Counter_tb is
 end Parking_Counter_tb;
 
 architecture rtl of Parking_Counter_tb is
 
-    
+    --Set component of Parking_Counter for testbench.
     component Parking_Counter
-    Port ( clk : in STD_LOGIC;
+    Port (
+        clk : in STD_LOGIC;
         reset : in STD_LOGIC;
         car_in : in STD_LOGIC;
         car_out : in STD_LOGIC;
-        count : out INTEGER range 0 to 15;
-        empty_count : out INTEGER range 0 to 15; --
-        empty : out STD_LOGIC; --
-        full : out STD_LOGIC);
+        count : out INTEGER range 0 to 18;
+        empty_count : out INTEGER range 0 to 18;
+        empty : out STD_LOGIC;
+        full : out STD_LOGIC
+    );
 end component;
 
-
+--Set signals use on testbench.
 signal clk : STD_LOGIC := '0';
 signal reset : STD_LOGIC := '0';
 signal car_in : STD_LOGIC := '0';
 signal car_out : STD_LOGIC := '0';
-signal count : INTEGER range 0 to 15;
-signal empty_count : INTEGER range 0 to 15; --
-signal empty : STD_LOGIC; --
+signal count : INTEGER range 0 to 18;
+signal empty_count : INTEGER range 0 to 18;
+signal empty : STD_LOGIC;
 signal full : STD_LOGIC;
 
+-- Set the clock(clk) signal period.
 constant clk_period : time := 10 ms;
---signal   finished    : std_logic := '0';
 
 begin
     
+    --Module Parking_Counter.
     uut: Parking_Counter
     Port map (
         clk => clk,
         reset => reset,
         car_in => car_in,
-        car_out => car_out,                 
+        car_out => car_out,
         count => count,
         empty_count => empty_count,
         empty => empty,
         full => full
     );
 
-    
+    --Generate a clock signal.
     clk_process :process
     begin
         clk <= '0';
@@ -56,63 +59,51 @@ begin
         wait for clk_period / 2;
     end process;
 
-    
+    --Stimulus Process
     stim_proc: process
     begin
+        --Reset Test
+        reset <= '1';  --reset
+        wait for clk_period;  --Waiting
+        reset <= '0';  --Unreset
         
-        reset <= '1';
-        wait for clk_period / 2;
-        reset <= '0';
-
-        
-        car_in <= '1';
-        wait for clk_period / 2;
-        car_in <= '0';
-        wait for clk_period / 2;
-
-        car_in <= '1';
-        wait for clk_period / 2;
-        car_in <= '0';
-        wait for clk_period / 2;
-
-        car_in <= '1';
-        wait for clk_period / 2;
-        car_in <= '0';
-        wait for clk_period / 2;
-
-        
-        car_out <= '1';
-        wait for clk_period / 2;
-        car_out <= '0';
-        wait for clk_period / 2;
-
-        
-        for i in 1 to 13 loop
-            car_in <= '1';
-            wait for clk_period / 2;
+        --state test of car in.
+        for i in 1 to 18 loop
+            car_in <= '1';  --carin
+            wait for clk_period; --Waiting
             car_in <= '0';
-            wait for clk_period / 2;
+            wait for clk_period; --Waiting
         end loop;
 
-        
-        wait for clk_period * 4;
+        --Test in the case where another car enters after 
+        --the parking lot is full (should not increase count)
+        car_in <= '1';  --carin
+        wait for clk_period;
+        car_in <= '0';
+        wait for clk_period;
 
-        
-        for i in 1 to 15 loop
-            car_out <= '1';
-            wait for clk_period / 2;
+        --If you enter and exit at the same time,
+        --the counter will be the same.
+        car_in <= '1';  --carin
+        car_out <= '1';  --carout
+        wait for clk_period;
+        car_in <= '0';
+        car_out <= '0';
+        wait for clk_period;
+
+        --state test of car out.
+        for i in 1 to 18 loop
+            car_out <= '1';  --carout
+            wait for clk_period; --Waiting
             car_out <= '0';
-            wait for clk_period / 2;
+            wait for clk_period; --Waiting
         end loop;
 
-        
-        reset <= '1';
-        wait for clk_period / 2;
-        reset <= '0';
-        wait for clk_period * 4;
+        reset <= '1';  --reset
+        wait for clk_period;
+        reset <= '0';  --unreset
 
         wait;
-        
     end process;
 
 end architecture rtl;
